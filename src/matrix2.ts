8f55150e731cfec86a6e6ce930559797e881bbe7
soloproyectos <gonzalo.chumillas@gmail.com>
2017-05-31 21:14:19 +0200

    radian degrees conversion

--- src/matrix2.ts
+++ src/matrix2.ts
@@ -248,3 +248,13 @@ export function getAngle(p: Positionable): number {
 
 	return angle;
 }
+
+// Converts [angle] to degrees.
+export function rad2deg(angle: number): number {
+	return 180 * angle / Math.PI;
+}
+
+// Converts [angle] to radians.
+export function deg2rad(angle: number): number {
+	return Math.PI * angle / 180;
+}
