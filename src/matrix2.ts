51c3ae9d0af7ea0dfc9618b5675468d37d22e172
soloproyectos <gonzalo.chumillas@gmail.com>
2017-09-05 00:13:17 +0200

    allow rotate around a point

--- src/matrix2.ts
+++ src/matrix2.ts
@@ -154,15 +154,17 @@ export class Transformation extends matrix.Transformation {
 		));
 	}
 
-	rotate(angle: number): Transformation {
+	rotate(angle: number, params?: {center: Point}): Transformation {
 		let ret: Transformation;
 		let cos = Math.cos(angle);
 		let sin = Math.sin(angle);
+		let center = params !== undefined? params.center: new Vector(0, 0);
+		let [x, y] = [center.x, center.y];
 
 		return this.transform(new Transformation(
 			new matrix.Vector(cos, sin, 0),
 			new matrix.Vector(-sin, cos, 0),
-			new matrix.Vector(0, 0, 1)
+			new matrix.Vector((1 - cos) * x + sin * y, (1 - cos) * y - sin * x, 1)
 		));
 	}
 
